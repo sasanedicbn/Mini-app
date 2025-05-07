e82449765187401c34a1814f935277ed9079bb6e
Sasa Nedic <svetasmak0@gmail.com>
2025-05-07 13:07:08 +0200

    Create BaseDataPicker reusable component

---
+++ Mini-app/src/components/UX/Reusable/BaseDataPicker.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="form-group">
+      <label v-if="label" :for="id">{{ label }}</label>
+      <div class="date-input-wrapper">
+        <input
+          :id="id"
+          type="date"
+          :value="modelValue"
+          @input="$emit('update:modelValue', $event.target.value)"
+          class="form-input"
+        >
+      </div>
+    </div>
+  </template>
+  
+  <script setup>
+  defineProps({
+    id: String,
+    label: String,
+    modelValue: String
+  })
+  
+  defineEmits(['update:modelValue'])
+  </script>
