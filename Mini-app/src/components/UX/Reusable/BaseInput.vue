2e54ab330ace9402f6d6d696a866718e06f0699c
Sasa Nedic <svetasmak0@gmail.com>
2025-05-07 12:53:40 +0200

    Create BaseInput component

--- Mini-app/src/components/UX/Reusable/BaseInput.vue
+++ Mini-app/src/components/UX/Reusable/BaseInput.vue
@@ -1,8 +1,27 @@
 <template>
-    
-
-</template>
-
-<script setup>
-
-</script>
+    <div class="form-group">
+      <label v-if="label" :for="id">{{ label }}</label>
+      <input
+        :id="id"
+        :type="type"
+        :value="modelValue"
+        @input="$emit('update:modelValue', $event.target.value)"
+        v-bind="$attrs"
+        class="form-input"
+      >
+    </div>
+  </template>
+  
+  <script setup>
+  defineProps({
+    id: String,
+    label: String,
+    type: {
+      type: String,
+      default: 'text'
+    },
+    modelValue: [String, Number]
+  })
+  
+  defineEmits(['update:modelValue'])
+  </script>
