e188132951d4567a300ef60e824efeba04fde9d7
Sasa Nedic <svetasmak0@gmail.com>
2025-05-07 13:06:17 +0200

    Create BaseSelect component

---
+++ Mini-app/src/components/UX/Reusable/BaseSelect.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="form-group">
+      <label v-if="label" :for="id">{{ label }}</label>
+      <div class="select-wrapper">
+        <select
+          :id="id"
+          :value="modelValue"
+          @change="$emit('update:modelValue', $event.target.value)"
+          class="form-input"
+        >
+          <option value="" disabled v-if="placeholder">{{ placeholder }}</option>
+          <option 
+            v-for="option in options" 
+            :key="option.value" 
+            :value="option.value"
+          >
+            {{ option.label }}
+          </option>
+        </select>
+      </div>
+    </div>
+  </template>
+  
+  <script setup>
+  defineProps({
+    id: String,
+    label: String,
+    placeholder: String,
+    modelValue: [String, Number],
+    options: {
+      type: Array,
+      default: () => [] 
+    }
+  })
+  
+  defineEmits(['update:modelValue'])
+  </script>
