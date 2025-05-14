47b6a1d96d0201ae6f51f71ee3eb599ab1a88971
Sasa Nedic <svetasmak0@gmail.com>
2025-05-14 23:04:22 +0200

    Clean file

--- server/server.js
+++ server/server.js
@@ -2,8 +2,6 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static("public"));
-
 app.get("/api", (req, res) => {
   res.json({ message: "Hello from server!" });
 });
