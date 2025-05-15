ccfda7169c65cce2630188f66f20d30555087d79
Sasa Nedic <svetasmak0@gmail.com>
2025-05-15 18:23:25 +0200

    Create get reques

--- server/server.js
+++ server/server.js
@@ -2,6 +2,10 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.get("/api", (req, res) => {
+  res.json({ message: "Hello from server!" });
+});
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
