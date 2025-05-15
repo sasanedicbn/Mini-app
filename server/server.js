b46e31b4f2df5b815ce940340d4b10d849e2dc74
Sasa Nedic <svetasmak0@gmail.com>
2025-05-15 18:22:31 +0200

    Create new port

--- server/server.js
+++ server/server.js
@@ -5,3 +5,7 @@ const PORT = process.env.PORT || 3000;
 app.get("/api", (req, res) => {
   res.json({ message: "Hello from server!" });
 });
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
