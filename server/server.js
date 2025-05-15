70dd594e889361fe1c00c83aa053bce75164b2fb
Sasa Nedic <svetasmak0@gmail.com>
2025-05-15 18:23:08 +0200

    Add commit

--- server/server.js
+++ server/server.js
@@ -2,10 +2,6 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get("/api", (req, res) => {
-  res.json({ message: "Hello from server!" });
-});
-
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
