6b6df67a0e087fc0bafac29d2e6390cba43576de
Sasa Nedic <svetasmak0@gmail.com>
2025-05-22 23:54:29 +0200

    Create changes

---
+++ Mini-app/src/components/Dashboard/DashboardHelpers/DashboardCalendar.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="calendar-grid">
+     <div class="day"></div>
+     <div class="day"></div>
+     <div class="day"></div>
+     <div class="day"></div>
+     <div class="day"></div>
+     <div class="day"></div>
+     <div class="day"></div>
+     <div class="day"></div>
+     <div class="day"></div>
+     <div class="day"></div>
+     <div class="day"></div>
+     <div class="day"></div>
+    </div>
+  </template>
+  
+  <style scoped>
+  .calendar-grid{
+    display: grid;
+    grid-template-columns: repeat(7, 1fr);
+    background-color: blue;
+    height: 500px;
+  }
+  .day{
+    height: 40px;
+    width: 40px;
+    border: 1px solid orange;
+    background-color: grey;
+  }
+  </style>
